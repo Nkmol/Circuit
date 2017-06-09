6b4d2b70fd890f637100d7596e5e1556c7aba239
Nkmol <sandermol95@hotmail.com>
2017-06-09 17:58:25 +0200

    Improve chain of Buillder and Parser - Parser and Builder are independent now - Controller is responsible for builder and parse cooperation

--- Circuit/Circuit.cs/BoardController.cs
+++ Circuit/Circuit.cs/BoardController.cs
@@ -13,16 +13,29 @@
 
         public void LoadBoard()
         {
-            var path = @"C:\Users\srm\Desktop\Circuit1_FullAdder.txt";
+            var path = @"C:\Users\Sander\Desktop\Circuit1_FullAdder.txt";
             var reader = new FileReader(path);
 
             boardParser = new BoardParser();
+            BoardBuilder bb = new BoardBuilder();
 
             try
             {
                 foreach (var line in reader.ReadLine())
                 {
-                    boardParser.Parse(line);
+                    if (boardParser.StartProbLinking)
+                    {
+                        var parserLink = boardParser.ParseLinkLine(line);
+                        if(parserLink != null) bb.LinkList(parserLink.Varname, parserLink.Values);
+                    }
+                    else
+                    {
+                        var component = boardParser.ParseVariableLine(line);
+                        if (component != null) bb.AddComponent(component.Varname, component.Compname, component.Input);
+                    }
+
+                    // TODO Improve this call
+                    _board = bb.Build();
                 }
             }
             catch (Exception e)
@@ -30,9 +43,6 @@
                 // TODO Move to view
                 Console.WriteLine(e);
             }
-
-            // TODO Improve this call
-            _board = boardParser.BoardBuilder.Board;
         }
 
         public void StartSimulation()
--- Circuit/Models/BoardBuilder.cs
+++ Circuit/Models/BoardBuilder.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
 namespace Models
 {
     public class BoardBuilder
@@ -7,18 +10,29 @@ namespace Models
         private readonly DirectGraph<Component> _nodes;
         private readonly ComponentFactory _componentFactory = new ComponentFactory();
 
-        public Board Board => new Board(_nodes);
-
         public BoardBuilder()
         {
             _nodes =  new DirectGraph<Component>();
         }
 
-        public void Link(string compName, string assignTo)
+        public Board Build()
         {
-            var component = _nodes[compName];
+            return new Board(_nodes);
+        }
 
-			_nodes[assignTo].LinkNext(component);
+        public void LinkList(string compName, IList<string> links)
+        {
+            foreach (var link in links)
+            {
+                Link(link, compName);
+            }
+        }
+
+        public void Link(string to, string from)
+        {
+            var component = _nodes[to];
+
+			_nodes[from].LinkNext(component);
         }
 
         public void AddComponent(string varName, string componentName, string input = "LOW")
