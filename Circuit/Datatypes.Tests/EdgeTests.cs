2db82eb29d6e927f7ba115da0475c6114466f5e0
Sander Mol <Sandermol95@hotmail.com>
2017-06-18 00:13:07 +0200

    Feature/tests (#29)
    * Refactor controller and parser
    
    * Refactor dgml generation
    - Use parser as middleware from writer to caller
    - Use strategy to use different parse strategies
    
    * Write Node name to DGML
    
    * Skips already checked backedges
    
    * Fix board in a board
    - Space inside a path works now
    - Automatically assign all inputs of sub-boards
    - Set the sub-board value to most occurnce probe value
    
    * Add edge tests

---
+++ Circuit/Datatypes.Tests/EdgeTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Datatypes.DirectedGraph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Datatypes.Tests
+{
+    [TestClass]
+    public class EdgeTests
+    {
+        // Quickfix to use graphnode
+        public class GraphNode : GraphNode<GraphNodeTests.GraphNode>
+        {
+        }
+
+        [TestMethod]
+        public void Edge_CompareEqualEdges_Equals()
+        {
+            // Arrange
+            GraphNode node1 = new GraphNode();
+            GraphNode node2 = new GraphNode();
+
+            Edge<GraphNode> edge1 = new Edge<GraphNode>(node1, node2);
+            Edge<GraphNode> edge2 = new Edge<GraphNode>(node1, node2);
+
+            // Act
+            var result = edge1.Equals(edge2);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Edge_CompareNotEqualsEdges_NotEquals()
+        {
+            // Arrange
+            GraphNode node1 = new GraphNode();
+            GraphNode node2 = new GraphNode();
+            GraphNode node3 = new GraphNode();
+
+            Edge<GraphNode> edge1 = new Edge<GraphNode>(node1, node2);
+            Edge<GraphNode> edge2 = new Edge<GraphNode>(node1, node3);
+
+            // Act
+            var result = edge1.Equals(edge2);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
